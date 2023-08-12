ac6cc5109b302dfddda2286ad23108b2b86d6e5c
Leo0114 <leonel0614@gmail.com>
2023-08-11 20:14:15 -0600

    ftProject

---
+++ src/data/projects.ts
@@ -0,0 +1,118 @@
+import NotFound from "@assets/404.webp";
+import Astro from "@assets/Astro.webp";
+import Citas from "@assets/Citas.webp";
+import Crypto from "@assets/Crypto.webp";
+import Gastos from "@assets/Gastos.webp";
+import Tareas from "@assets/Tareas.webp";
+import School from "@assets/School.webp";
+import Cabana from "@assets/cabanabanner.webp"
+export const procesarProjects = (estancias: any) => {};
+
+export const projects = [
+  {
+    title:"",
+    notes:"",
+    img: Citas,
+    alt: "Agendar Citas Web",
+    github: "https://github.com/Leo0114/citas-react",
+    href: "https://leo0114.github.io/citas-react/",
+    frame: "React",
+    style: "Css",
+  },
+  {
+    img: Crypto,
+    alt: "Crypto Calculate Web",
+    github: "https://github.com/Leo0114/cripto",
+    href: "https://leo0114.github.io/cripto/",
+    frame: "React",
+    style: "Css",
+  },
+  {
+    img: Gastos,
+    alt: "Gastos Web",
+    github: "https://github.com/Leo0114/gastos",
+    href: "https://leo0114.github.io/gastos/",
+    frame: "React",
+    style: "Css",
+  },
+  {
+    img: Tareas,
+    alt: "Crear tarea Web",
+    github: "https://github.com/Leo0114/react-example",
+    href: "https://leo0114.github.io/react-example/",
+    frame: "React",
+    style: "Tailwindcss",
+  },
+  {
+    img: Astro,
+    alt: "Probando Astro Web",
+    github: "https://github.com/Leo0114/astro-landingpage",
+    href: "https://leo0114.github.io/astro-landingpage/",
+    frame: "Astro",
+    style: "Tailwindcss",
+  },
+  {
+    img: Cabana,
+    alt: "Cabañas Web",
+    github: "https://github.com/Leo0114/volcancafe",
+    href: "https://leo0114.github.io/volcancafe/",
+    frame: "Astro",
+    style: "Tailwindcss",
+  },
+  {
+    img: NotFound,
+    alt: "Demo PA Web",
+    github: "https://github.com/Leo0114/DemoPA",
+    href: "https://leo0114.github.io/DemoPA/",
+    frame: "Astro",
+    style: "Tailwindcss",
+  },
+  {
+    img: NotFound,
+    alt: "Notes Web",
+    github: "https://github.com/Leo0114/NotesApp",
+    href: "https://leo0114.github.io/NotesApp/",
+    frame: "Astro",
+    style: "Tailwindcss",
+  },
+  {
+    img: NotFound,
+    alt: "Netflix clone Web",
+    github: "https://github.com/Leo0114/NetflixApp",
+    href: "https://leo0114.github.io/NetflixApp/",
+    frame: "Astro",
+    style: "Tailwindcss",
+  },
+  {
+    img: NotFound,
+    alt: "Shopping Web",
+    github: "https://github.com/Leo0114/ShoppingApp",
+    href: "https://leo0114.github.io/ShoppingApp/",
+    frame: "Astro",
+    style: "Tailwindcss",
+  },
+  {
+    img: NotFound,
+    alt: "Chair Web",
+    github: "https://github.com/Leo0114/ChairApp",
+    href: "https://leo0114.github.io/ChairApp/",
+    frame: "Astro",
+    style: "Tailwindcss",
+  },
+  {
+    img: NotFound,
+    alt: "Experiences Web",
+    github: "https://github.com/Leo0114/Experiences",
+    href: "https://leo0114.github.io/Experiences/",
+    frame: "Astro",
+    style: "Tailwindcss",
+  },
+  {
+    img: School,
+    alt: "School Project Web",
+    github: "https://github.com/Leo0114/Terceraparcial",
+    href: "https://leo0114.github.io/Terceraparcial/index.html",
+    frame: "Html",
+    style: "Materialize",
+  },
+];
