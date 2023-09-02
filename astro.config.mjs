ab55fe9d44fa70c30c8165e6f9b47111d3f2c084
Leo0114 <leonel0614@gmail.com>
2023-09-02 04:27:54 -0600

    velcel Config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,13 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 
+import vercel from "@astrojs/vercel/serverless";
+
 // https://astro.build/config
 export default defineConfig({
   site: "https://Leo0114.github.io",
   base: "/Portfolio",
   integrations: [tailwind()],
+  output: "server",
+  adapter: vercel()
 });
